1200777b0bc58c3af2200364655e1ae036db0b1e
Nataliagomez011 <gomezrojasnatalia@hotmail.es>
2024-03-27 14:54:43 -0500

    Segundo commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,24 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { ThemeContext } from './utils/global.context'
-import "../Styles/Navbar.css"
+import { ThemeContext } from './utils/global.context';
+import '../Styles/Navbar.css';
 
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
-
-function Navbar() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-
-  const changeTheme = () => {
-    toggleTheme(); // Método para cambiar el tema del contexto global
-  };
+const Navbar = () => {
+  const { toggleTheme } = useContext(ThemeContext);
 
   return (
-    <nav className={theme === 'dark' ? 'darkTheme' : 'lightTheme'}>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-
-      <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/favs">Favs</Link></li>
-        <li><Link to="/contacto">Contacto</Link></li>
-      </ul>
-
-      <button onClick={changeTheme}>Change theme</button>
+    <nav className="navbar">
+      <div className="brand">DH Odonto</div>
+      <div className="nav-links">
+        <Link to="/home" className="nav-link">Home</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
+        <Link to="/favs" className="nav-link">Favs</Link>
+      </div>
+      <div className="theme-toggle" onClick={toggleTheme}>
+        🌙 
+      </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
